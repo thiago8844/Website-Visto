c52576d58af6f488833fae2654e5fa96fd7d9cf6
Thiago Machado <thiagoaxe4@gmail.com>
2024-10-14 21:16:22 -0300

    modificando rota para o github pages

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,9 @@ import vituum from "vituum";
 import twig from "@vituum/vite-plugin-twig";
 
 export default {
+  base: "/Website-Visto/",
   build: {
-    outDir: "docs", // Change output directory to 'docs'
+    outDir: "docs",
   },
   plugins: [vituum(), twig({ root: "./src" })],
 };
